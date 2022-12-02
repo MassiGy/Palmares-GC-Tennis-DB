22b554de3a83e24fb0bbab4830b5497749d1efcd
Clément FLAMBARD <clement.flambard@gmail.com>
2022-12-02 12:36:54 +0100

    +postgresql: renamed files

--- src/postgresql/index.sql
+++ src/postgresql/index.sql
@@ -1,3 +1,9 @@
+/*
 SELECT Player_Last_Name
-FROM Player NATURAL JOIN Participate NATURAL JOIN Grand_Slam
+FROM P16_Player NATURAL JOIN P16_Participate NATURAL JOIN P16_Grand_Slam
 WHERE GC_Name = 'Rolland Garros' AND _Year = 2022 AND Result = 'Champion';
+*/
+
+SELECT DISTINCT Player_First_Name, Player_Last_Name, Player_ATP_Rank
+FROM P16_Player NATURAL JOIN P16_Participate NATURAL JOIN P16_Grand_Slam
+WHERE Result IN ('1/4 Finalist', '1/2 Finalist', 'Runner-up', 'Champion');

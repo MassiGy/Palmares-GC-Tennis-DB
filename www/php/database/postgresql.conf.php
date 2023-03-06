146795f49d965e6fb06634a59f36a7d14c1a9740
Clement Flambard <fc182724@localhost.univlehavre.lan>
2023-03-06 17:31:08 +0100

    +dev-gohmma : overall pg_connection done

--- www/php/database/postgresql.conf.php
+++ www/php/database/postgresql.conf.php
@@ -1,6 +1,6 @@
 <?php
     include "../env.php";
 
-    $strConnection = "host=" . strval($_ENV["DB_HOST"]) . " dbname=" . strval($_ENV["DB_NAME"]) . " user=" . strval($_ENV["DB_USER"] . " password=" . strval($_ENV["DB_PASSWORD"]));
-    var_dump($strConnection);
+    $strConnection = "host=" . strval($_ENV["DB_HOST"]) . " dbname=" . strval($_ENV["DB_NAME"]) 
+    . " user=" . strval($_ENV["DB_USER"] . " password=" . strval($_ENV["DB_PASSWORD"]));
     $DB_connect = pg_connect($strConnection);

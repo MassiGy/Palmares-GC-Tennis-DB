db30658771ecf8b245520d2cf0af2c0c0e2275f0
MassiGy <ghernaoutmassi@gmail.com>
2023-03-06 16:19:22 +0100

    +dev-massigy: added docs to sanitizer, and added a blank controller to postgres/controllers

--- www/php/utils/sanitize.php
+++ www/php/utils/sanitize.php
@@ -1,6 +1,14 @@
 <?php
 
 
+
+/**
+ * @description: sanitizer, allows us to to sanitize the REQUEST/RESPONSE objects, 
+ * 
+ * @param string $data, which is data to sanitize
+ * @return string, which is the sanitized data.
+ * 
+ */
 function sanitizer(string $data): string
 {
     $data = htmlspecialchars($data);

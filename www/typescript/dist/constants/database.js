17f6a52abbd89dc0b58c64c74c5f5fbf522d1720
MassiGy <ghernaoutmassi@gmail.com>
2025-06-01 15:57:38 +0200

    gitignore was messed up

--- www/typescript/dist/constants/database.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-module.exports.database = {
-    dialect: "mysql",
-    tables: ["P16_Player", "P16_Participate", "P16_Grand_Slam"],
-};
--- www/typescript/dist/controllers/get_not_found_page.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-module.exports.get_not_found_page = (req, res) => {
-    res.render("404.ejs");
-};
